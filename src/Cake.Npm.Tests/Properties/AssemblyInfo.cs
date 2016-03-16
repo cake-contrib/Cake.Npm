1bfc27a477aae801cf427af20555f6c3009d7c18
nengberg <niklas.engberg@creuna.se>
2016-03-16 09:47:16 +0100

    adds initial test project.

---
+++ src/Cake.Npm.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cake.Npm.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cake.Npm.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: ComVisible(false)]
