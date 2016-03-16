d5adfc063f2360ffacf19685f00bb89c12b63252
nengberg <niklas.engberg@creuna.se>
2016-03-16 10:09:34 +0100

    Adds unit test for the most common scenarios.

---
+++ src/Cake.Npm.Tests/NpmRunnerFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Cake.Testing.Fixtures;
+
+namespace Cake.Npm.Tests {
+	public class NpmRunnerFixture : ToolFixture<NpmInstallSettings> {
+		public NpmRunnerFixture() : base("npm") { }
+
+		public Action<NpmInstallSettings> InstallSettings { get; set; }
+
+		protected override void RunTool() {
+			var tool = new NpmRunner(FileSystem, Environment, ProcessRunner, Globber);
+			tool.Install(InstallSettings);
+		}
+	}
+}
