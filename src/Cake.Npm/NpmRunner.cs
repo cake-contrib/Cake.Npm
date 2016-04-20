0238de49691198b9f5ef0c6f04f7419609b80f96
Alistair Chapman <alistairgchapman@gmail.com>
2016-04-20 19:45:16 +1000

    Change whitespace to be more consistent

--- src/Cake.Npm/NpmRunner.cs
+++ src/Cake.Npm/NpmRunner.cs
@@ -6,81 +6,81 @@ using Cake.Core.Tooling;
 
 namespace Cake.Npm
 {
-	/// <summary>
-	/// A wrapper around the Node Npm package manager
-	/// </summary>
-	public class NpmRunner : Tool<NpmRunnerSettings>
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="NpmRunner" /> class.
-		/// </summary>
-		/// <param name="fileSystem">The file system</param>
-		/// <param name="environment">The environment</param>
-		/// <param name="processRunner">The process runner</param>
-		/// <param name="globber">The globber</param>
-		internal NpmRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IGlobber globber) : base(fileSystem, environment, processRunner, globber)
-		{
-		}
-		
-		/// <summary>
-		/// execute 'npm install' with options
-		/// </summary>
-		/// <param name="configure">options when running 'npm install'</param>
-		public void Install(Action<NpmInstallSettings> configure = null)
-		{
-			var settings = new NpmInstallSettings();
-			configure?.Invoke(settings);
+    /// <summary>
+    /// A wrapper around the Node Npm package manager
+    /// </summary>
+    public class NpmRunner : Tool<NpmRunnerSettings>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NpmRunner" /> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system</param>
+        /// <param name="environment">The environment</param>
+        /// <param name="processRunner">The process runner</param>
+        /// <param name="globber">The globber</param>
+        internal NpmRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IGlobber globber) : base(fileSystem, environment, processRunner, globber)
+        {
+        }
+        
+        /// <summary>
+        /// execute 'npm install' with options
+        /// </summary>
+        /// <param name="configure">options when running 'npm install'</param>
+        public void Install(Action<NpmInstallSettings> configure = null)
+        {
+            var settings = new NpmInstallSettings();
+            configure?.Invoke(settings);
 
-			var args = GetNpmInstallArguments(settings);
+            var args = GetNpmInstallArguments(settings);
 
-			Run(settings, args);
-		}
+            Run(settings, args);
+        }
 
-		private ProcessArgumentBuilder GetNpmInstallArguments(NpmInstallSettings settings)
-		{
-			var args = new ProcessArgumentBuilder();
-			settings.Evaluate(args);
-			return args;
-		}
+        private ProcessArgumentBuilder GetNpmInstallArguments(NpmInstallSettings settings)
+        {
+            var args = new ProcessArgumentBuilder();
+            settings.Evaluate(args);
+            return args;
+        }
 
-		/// <summary>
-		/// execute 'npm run'/'npm run-script' with arguments
-		/// </summary>
-		/// <param name="scriptName">name of the </param>
-		/// <param name="configure"></param>
-		public void RunScript(string scriptName, Action<NpmRunScriptSettings> configure = null)
-		{
-			var settings = new NpmRunScriptSettings(scriptName);
-			configure?.Invoke(settings);
-			var args = GetNpmRunArguments(settings);
+        /// <summary>
+        /// execute 'npm run'/'npm run-script' with arguments
+        /// </summary>
+        /// <param name="scriptName">name of the </param>
+        /// <param name="configure"></param>
+        public void RunScript(string scriptName, Action<NpmRunScriptSettings> configure = null)
+        {
+            var settings = new NpmRunScriptSettings(scriptName);
+            configure?.Invoke(settings);
+            var args = GetNpmRunArguments(settings);
 
-			Run(settings, args);
-		}
+            Run(settings, args);
+        }
 
-		private ProcessArgumentBuilder GetNpmRunArguments(NpmRunScriptSettings settings)
-		{
-			var args = new ProcessArgumentBuilder();
-			settings.Evaluate(args);
-			return args;
-		}
+        private ProcessArgumentBuilder GetNpmRunArguments(NpmRunScriptSettings settings)
+        {
+            var args = new ProcessArgumentBuilder();
+            settings.Evaluate(args);
+            return args;
+        }
 
-		/// <summary>
-		/// Gets the name of the tool
-		/// </summary>
-		/// <returns>the name of the tool</returns>
-		protected override string GetToolName()
-		{
-			return "Npm Runner";
-		}
+        /// <summary>
+        /// Gets the name of the tool
+        /// </summary>
+        /// <returns>the name of the tool</returns>
+        protected override string GetToolName()
+        {
+            return "Npm Runner";
+        }
 
-		/// <summary>
-		/// Gets the name of the tool executable
-		/// </summary>
-		/// <returns>The tool executable name</returns>
-		protected override IEnumerable<string> GetToolExecutableNames()
-		{
-			yield return "npm.cmd";
-			yield return "npm";
-		}
-	}
+        /// <summary>
+        /// Gets the name of the tool executable
+        /// </summary>
+        /// <returns>The tool executable name</returns>
+        protected override IEnumerable<string> GetToolExecutableNames()
+        {
+            yield return "npm.cmd";
+            yield return "npm";
+        }
+    }
 }
