5f28f1371b1439ae0e6b52fa5c3494a4487adbd0
Pascal Berger <pascal.berger@gmail.com>
2018-09-11 20:55:57 +0200

    (GH-90) Add NpmCi alias which accepts a settings configurator

--- src/Cake.Npm/NpmCiAliases.cs
+++ src/Cake.Npm/NpmCiAliases.cs
@@ -36,6 +36,38 @@ namespace Cake.Npm
         }
 
         /// <summary>
+        /// Cis packages using the settings returned by a configurator.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="configurator">The settings configurator.</param>
+        /// <example>
+        /// <para>Use specific log level ('npm ci')</para>
+        /// <code>
+        /// <![CDATA[
+        ///     NpmCi(settings => settings.WithLogLevel(NpmLogLevel.Verbose));
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Ci")]
+        public static void NpmCi(this ICakeContext context, Action<NpmCiSettings> configurator)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (configurator == null)
+            {
+                throw new ArgumentNullException(nameof(configurator));
+            }
+
+            var settings = new NpmCiSettings();
+            configurator(settings);
+            context.NpmCi(settings);
+        }
+
+        /// <summary>
         /// Cis packages using the specified settings.
         /// </summary>
         /// <param name="context">The context.</param>
